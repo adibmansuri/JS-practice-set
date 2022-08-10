1c456f1da2227f0be27e377fccdd3bd9cf42f3c4
Adib <mansuri.adib25@gmail.com>
2022-08-10 19:02:27 +0200

    js lern challange2

--- script.js
+++ script.js
@@ -14,12 +14,21 @@
 // console.log(JSisfun);
 // console.log(typeof true);
 
-const mass = 78;
+const mass = 92;
 const hight = 1.69;
 const johnBmi = mass / hight ** 2;
 
-const markmass = 92;
+const markmass = 52;
 const markhight = 1.95;
 const markBmi = markmass / markhight ** 2;
 const check = johnBmi > markBmi;
 console.log(johnBmi, markBmi, check);
+
+// challange 1
+let challange;
+if (johnBmi > markBmi) {
+  challange = `Mark's BMI (${markBmi})is higher then John's BMI (${johnBmi}) !`;
+} else {
+  challange = `johans's BMI (${johnBmi})is higher then mark's BMI(${markBmi}) !`;
+}
+console.log(challange);
